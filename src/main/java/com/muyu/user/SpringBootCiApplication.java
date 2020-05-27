465091f6957fa5e88d9efb9d9a84c69e4bb0be63
Zhao Fusen <fusen.zhao@foxmail.com>
2020-05-27 11:10:49 +0800

    init first commit for sprintboot container ci

---
+++ src/main/java/com/muyu/user/SpringBootCiApplication.java
@@ -0,0 +1,13 @@
+package com.muyu.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muyu/user/SpringBootCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muyu.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
